941fd9fded2481dc444557ed42ccf1b52819ae8a
♥ Gem Stacy ♥ <gem.stacy.a@gmail.com>
2019-03-03 22:41:06 -0800

    brain achey

---
+++ cycles jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game over");
+            // Restart Game
+        }
+    }
+}
--- cycles jam/Assets/Scripts/PlayerCollision.cs
+++ cycles jam/Assets/Scripts/PlayerCollision.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
     {
-        
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
     }
 }
