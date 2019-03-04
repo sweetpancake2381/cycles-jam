54e658f399575aaf63831ef77bab505fd50a7d05
♥ Gem Stacy ♥ <gem.stacy.a@gmail.com>
2019-03-03 17:50:26 -0800

    starting to add animation

--- cycles jam/Assets/PlayerMovement.cs
+++ cycles jam/Assets/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour{
 
     float horizontalMove = 0f;
     bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
@@ -21,13 +22,21 @@ public class PlayerMovement : MonoBehaviour{
             jump = true;
 
         }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp)"Crouch"))
+        {
+            crouch = false;
+
+        }
 
     }
 
     void FixedUpdate ()
     {
         //Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
 }
